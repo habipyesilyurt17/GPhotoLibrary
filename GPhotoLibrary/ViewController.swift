ab16061632c370a54553ea9698ec40cea4cf6329
Habip Yeşilyurt <yesilyurthabip@hotmail.com>
2025-05-23 14:35:09 +0300

    Initial Commit

---
+++ GPhotoLibrary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPhotoLibrary
+//
+//  Created by Habip on 23.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
